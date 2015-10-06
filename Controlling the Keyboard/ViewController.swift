5c2397e6be078ea574412bfede3f7867c1ad86d5
Luiz Fernando Santiago <luizfso@gmail.com>
2015-10-06 13:52:56 -0300

    My Code for Keyboard

--- Controlling the Keyboard/ViewController.swift
+++ Controlling the Keyboard/ViewController.swift
@@ -8,18 +8,46 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITextFieldDelegate {
 
+    
+    
+    @IBOutlet weak var resultLabel: UILabel!
+    
+    @IBOutlet weak var textField: UITextField!
+    
+    @IBAction func actionButton(sender: AnyObject) {
+        
+        resultLabel.text = textField.text
+        textField.resignFirstResponder()
+        
+        
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        self.textField.delegate = self
+        
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    override func touchesBegan(touches: Set<UITouch>, withEvent event: UIEvent?) {
+        
+        self.view.endEditing(true)
+        
+    }
+    
+    func textFieldShouldReturn(textField: UITextField) -> Bool{
+        
+        textField.resignFirstResponder()
+        return true
+        
+    }
 }
 
